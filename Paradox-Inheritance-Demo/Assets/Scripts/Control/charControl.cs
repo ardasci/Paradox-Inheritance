d658c1381eb18a2fdb0587836f1fd23e8714f56f
oguzkaansarikaya <oguzkaansarikaya@hotmail.com>
2023-06-11 19:44:26 +0300

    Movement is fixed & FPS camera is added & Animation is added
    There are some bugs on animation and camera. Need to be fix.

--- Paradox-Inheritance-Demo/Assets/Scripts/Control/charControl.cs
+++ Paradox-Inheritance-Demo/Assets/Scripts/Control/charControl.cs
@@ -5,37 +5,119 @@ using UnityEngine.InputSystem;
 
 public class charControl : MonoBehaviour
 {
-    public Rigidbody charRb;
-    public Vector2 moveVal;
-    public float moveSpeed = 1;
-    public Vector3 jumpVal;
-    public float jumpForce = 10;
+    private PlayerInput playerInput;
+    private Rigidbody charRb;
+    private Animator charAnim;
+    public Vector2 moveInput;
+    public Vector3 currentMove;
+    public float moveSpeed = 2;
+    public float jumpForce = 4;
+    public bool isMovePressed;
+    public bool onGround;
+    
 
-    private void Awake()
+    private void Start()
     {
         charRb = GetComponent<Rigidbody>();
+        charAnim = GetComponent<Animator>();
+    }
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        playerInput.playerController.Movement.started += Move;
+        playerInput.playerController.Movement.performed += Move;
+        playerInput.playerController.Movement.canceled += Move;
     }
 
-    public void moving(InputAction.CallbackContext value)
+    void Move(InputAction.CallbackContext context)
     {
-        if (value.performed)
-        {
-            moveVal = value.ReadValue<Vector2>();
-        }
+        moveInput = context.ReadValue<Vector2>();
+        currentMove.x = moveInput.x;
+        currentMove.z = moveInput.y;
+        isMovePressed = moveInput.x != 0 || moveInput.y != 0;
+    }
 
-        if (value.canceled)
+    private void FixedUpdate()
+    {
+        if (isMovePressed)
         {
-            moveVal = value.ReadValue<Vector2>();
+            charRb.MovePosition(transform.position + currentMove * moveSpeed * Time.deltaTime);
         }
+        if (moveInput.y == 0)
+        {
+            charAnim.SetFloat("movement", 0);
+            charAnim.SetFloat("vertical", 0);
+            charAnim.SetFloat("horizontal", 0);
+        }
+        else
+        {
+            charAnim.SetFloat("movement", 1);
+
+            switch (moveInput.y)
+            {
+                case 1:
+                    charAnim.SetFloat("vertical",1);
+                    break;
+
+                case -1:
+                    charAnim.SetFloat("vertical", -1);
+                    break;
+
+                case 0:
+                    break;
+            }
+
+            
+        }
+    }
+    private void OnEnable()
+    {
+        playerInput.playerController.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.playerController.Disable();
     }
 
     public void jump(InputAction.CallbackContext value)
     {
-        if (value.performed)
+        if (value.performed && onGround)
         {
-            Debug.Log("zýpla");
             charRb.AddForce(jumpForce*Vector3.up,ForceMode.Impulse);
+
         }
         
+    }
+
+    public void spirnt(InputAction.CallbackContext value)
+    {
+        if (value.performed && moveInput.y == 1)
+        {
+            moveSpeed = 3;
+        }
+
+        if (value.canceled)
+        {
+            moveSpeed = 2;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "plane")
+        {
+            onGround = true;
+            charAnim.SetBool("jump",false);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.tag == "plane")
+        {
+            onGround = false;
+            charAnim.SetBool("jump", true);
+        }
     }
 }
