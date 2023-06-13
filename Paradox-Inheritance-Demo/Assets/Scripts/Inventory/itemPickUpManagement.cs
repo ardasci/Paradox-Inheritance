391cfe769651cf2042e3859cb4e521673e38b760
oguzkaansarikaya <oguzkaansarikaya@hotmail.com>
2023-06-13 05:40:31 +0300

    Bugs are fixed.

--- Paradox-Inheritance-Demo/Assets/Scripts/Inventory/itemPickUpManagement.cs
+++ Paradox-Inheritance-Demo/Assets/Scripts/Inventory/itemPickUpManagement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class itemPickUpManagement : MonoBehaviour
 {
     [SerializeField] GameObject pickGuide;
+    private bool inventoryControl;
     private GameObject collectibleObject;
     private int starCount;
     private int aidCount;
@@ -15,72 +16,17 @@ public class itemPickUpManagement : MonoBehaviour
         collectibleObject = gameObject;
     }
 
+    private void Update()
+    {
+        pickUp();
+    }
     private void OnTriggerStay(Collider other)
     {
 
         if (other.tag == "Player")
         {
             pickGuide.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.E))
-            {
-                switch (collectibleObject.tag)
-                {
-                    case "star":
-                        if (PlayerPrefs.HasKey("star"))
-                        {
-                            starCount = PlayerPrefs.GetInt("star");
-                            starCount++;
-                            PlayerPrefs.SetInt("star", starCount);
-                        }
-                        else
-                        {
-                            starCount++;
-                            PlayerPrefs.SetInt("star", starCount);
-                        }
-                        break;
-                    case "coin":
-                        if (PlayerPrefs.HasKey("coin"))
-                        {
-                            coinCount = PlayerPrefs.GetInt("coin");
-                            coinCount++;
-                            PlayerPrefs.SetInt("coin", coinCount);
-                        }
-                        else
-                        {
-                            coinCount++;
-                            PlayerPrefs.SetInt("coin", coinCount);
-                        }
-                        break;
-                    case "boat":
-                        if (PlayerPrefs.HasKey("boat"))
-                        {
-                            boatCount = PlayerPrefs.GetInt("boat");
-                            boatCount++;
-                            PlayerPrefs.SetInt("boat", boatCount);
-                        }
-                        else
-                        {
-                            boatCount++;
-                            PlayerPrefs.SetInt("boat", boatCount);
-                        }
-                        break;
-                    case "aidKit":
-                        if (PlayerPrefs.HasKey("aid"))
-                        {
-                            aidCount = PlayerPrefs.GetInt("aid");
-                            aidCount++;
-                            PlayerPrefs.SetInt("aid", aidCount);
-                        }
-                        else
-                        {
-                            aidCount++;
-                            PlayerPrefs.SetInt("aid", aidCount);
-                        }
-                        break;
-                }
-                Destroy(collectibleObject);
-                pickGuide.SetActive(false);
-            }
+            inventoryControl = true;
         }
 
     }
@@ -88,5 +34,70 @@ public class itemPickUpManagement : MonoBehaviour
     private void OnTriggerExit(Collider other)
     {
         pickGuide.SetActive(false);
+        inventoryControl = false;
+    }
+
+    public void pickUp()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inventoryControl)
+        {
+            switch (collectibleObject.tag)
+            {
+                case "star":
+                    if (PlayerPrefs.HasKey("star"))
+                    {
+                        starCount = PlayerPrefs.GetInt("star");
+                        starCount++;
+                        PlayerPrefs.SetInt("star", starCount);
+                    }
+                    else
+                    {
+                        starCount++;
+                        PlayerPrefs.SetInt("star", starCount);
+                    }
+                    break;
+                case "coin":
+                    if (PlayerPrefs.HasKey("coin"))
+                    {
+                        coinCount = PlayerPrefs.GetInt("coin");
+                        coinCount++;
+                        PlayerPrefs.SetInt("coin", coinCount);
+                    }
+                    else
+                    {
+                        coinCount++;
+                        PlayerPrefs.SetInt("coin", coinCount);
+                    }
+                    break;
+                case "boat":
+                    if (PlayerPrefs.HasKey("boat"))
+                    {
+                        boatCount = PlayerPrefs.GetInt("boat");
+                        boatCount++;
+                        PlayerPrefs.SetInt("boat", boatCount);
+                    }
+                    else
+                    {
+                        boatCount++;
+                        PlayerPrefs.SetInt("boat", boatCount);
+                    }
+                    break;
+                case "aidKit":
+                    if (PlayerPrefs.HasKey("aid"))
+                    {
+                        aidCount = PlayerPrefs.GetInt("aid");
+                        aidCount++;
+                        PlayerPrefs.SetInt("aid", aidCount);
+                    }
+                    else
+                    {
+                        aidCount++;
+                        PlayerPrefs.SetInt("aid", aidCount);
+                    }
+                    break;
+            }
+            Destroy(collectibleObject);
+            pickGuide.SetActive(false);
+        }
     }
 }
